6f78d7b46b7df014d7939cb492a981fa439d07b7
dyants <ciptoardianto70@gmail.com>
2025-11-20 16:22:50 +0700

    update

--- js/main.js
+++ js/main.js
@@ -24,9 +24,78 @@ document.addEventListener("click", function (event) {
 
 // Navbar untuk efek scroll
 window.addEventListener("scroll", function () {
-  const navbar = this.document.getElementById("navbar");
-  if (windows.scrolly > 50) {
+  const navbar = document.getElementById("navbar");
+  if (window.scroll > 50) {
+    navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
   }
 });
+
+// smoth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  });
+});
+
+// Slider Testimoni
+const track = document.getElementById("testimonialsTrack");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+let currentIndex = 0;
+const cards = document.querySelectorAll(".testimonial-card");
+const totalCard = cards.length;
+
+function updateSlider() {
+  const carWidth = cards[0].offsetWidth;
+  const gap = 32; //2 rem gap
+  track.style.transform = `translateX(-${currentIndex * (carWidth + gap)}px)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % totalCard;
+  updateSlider();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + totalCard) % totalCard;
+  updateSlider();
+});
+
+// slide otomatis
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % totalCards;
+  updateSlider();
+}, 5000);
+
+// scroll animasi
+const revealElements = document.querySelectorAll(".scroll-reveal");
+const revealObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("revealed");
+      }
+    });
+  },
+
+  {
+    threshold: 0.1,
+    rootMargin: "0px 0px -50px 0px",
+  }
+);
+
+revealElements.forEach((el) => {
+  revealObserver.observe(el);
+});
+
+// responsive slider
+window.addEventListener("resize", updateSlider);
